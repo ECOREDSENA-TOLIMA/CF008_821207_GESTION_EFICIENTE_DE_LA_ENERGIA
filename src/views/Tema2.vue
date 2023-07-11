c1ff72aa084a8280ef54617634350c3203e5d97f
Oscar Espitia <odespitia@sena.edu.co>
2023-07-11 01:29:33 -0500

    tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -2,10 +2,147 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-botones
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Potencial de los sistemas energéticos renovables
+
+    p.mb-4(data-aos="fade-down") Conocer el potencial de los sistemas energéticos renovables.
+
+    .tarjeta.tarjeta--azul.p-4.mb-5
+      SlyderA(tipo="b")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Potencial de los sistemas energéticos renovables.
+            p Actualmente existen varias tecnologías que permiten la obtención de energía final en forma de electricidad, calor y combustibles a partir de las Fuentes de Energía Renovable. A través de los años varias de estas tecnologías han madurado y se convirtieron en soluciones técnicas y comerciales para las necesidades energéticas de los países. En Colombia, se han implementado proyectos de generación con tecnologías solar fotovoltaica, solar térmica, también el uso del bagazo de caña como fuente primaria para efectos de cogeneración, y en el desarrollo de proyectos de generación de electricidad con energía eólica.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/tema2-01.png', alt='')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 El potencial de generación para cada una de las tecnologías con fuentes renovables es diferente, ya que depende de las siguientes condiciones en el lugar de instalación:
+            ul.lista-ul.mb-2
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Disponibilidad del recurso natural a utilizar.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Condiciones climáticas.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Condiciones ambientales.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Condiciones geográficas.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Condiciones de logística para acceso al lugar.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Información meteorológica disponible.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Condiciones culturales y sociales.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Condiciones de interconexión con redes de suministro existentes.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/tema2-02.png', alt='')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Debido a lo anterior
+            p En Colombia se han elaborado diferentes documentos que recopilan toda la información requerida para la evaluación del potencial de diferentes fuentes de energía en las regiones del país. Estos documentos se denominan con el nombre “Atlas” y se encuentran publicados en las páginas web del Instituto de Hidrología, Meteorología y Estudios Ambientales - IDEAM y de la Unidad de Planeación Minero Energética-UPME.  A través de estos Atlas se facilita la identificación de regiones que resultan estratégicas por los potenciales recursos para la solución de necesidades energéticas de la población. 
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/tema2-03.png', alt='')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 De manera general, la información consignada en los Atlas se compone de:
+            p Conjunto de mapas climatológicos con la información de la distribución promedio mensual y anual de las variables de interés.
+            ul.lista-ul.mb-2
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Aspectos teóricos y observaciones para la interpretación de los datos consignados en los mapas.
+              li.mb-0
+                i.fas.fa-bolt(style="color:#AD84FF")
+                | Anexos con documentación de interés. 
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/tema2-04.png', alt='')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Cabe resaltar:
+            p Que la información consignada en los Atlas sirve como referencia para analizar el potencial general del uso de cierta tecnología que utiliza como fuente primaria la energía renovable.
+            p Cuando se decide realizar un proyecto en particular, se deben realizar mediciones en sitio con instrumentos de medida con una duración en tiempos especificados por el proyecto. Adicionalmente se deben tener en cuenta de manera particular las condiciones del lugar de instalación mencionadas anteriormente.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/tema2-05.png', alt='')
+
+    p.mb-4(data-aos="fade-down") A continuación, se describe el potencial de generación en dos fuentes de energía renovable no convencional: energía solar y energía eólica.
+
+    Separador
+    #t_2_1.titulo-segundo.color-acento-botones(data-aos="fade-right")
+      h2 2.1 Potencial Energético de la Energía solar
+
+    p.mb-4(data-aos="fade-down") Revisar el potencial energético de la energía solar.
+    p.mb-4(data-aos="fade-down") #[strong Potencial Energético de la Energía solar]
+    
+    p.mb-4(data-aos="fade-down") El aprovechamiento de la energía solar generalmente se da en dos aplicaciones: energía solar fotovoltaica y energía solar térmica. A través de la aplicación solar fotovoltaica se aprovecha la energía del sol para generar electricidad, y por medio de las aplicaciones solares térmicas es posible elevar la temperatura del agua u otros fluidos de tal forma que se pueda aprovechar esa temperatura en diferentes usos de la energía.
+
+    p.mb-4(data-aos="fade-down") De acuerdo con IDEAM, UPME (2017), la energía que proviene del sol llega a la tierra en forma de radiación solar, la cual se define como la energía emitida por el sol, que se propaga en todas las direcciones a través del espacio mediante ondas electromagnéticas y es generada en las reacciones del hidrógeno en el núcleo del sol por fusión nuclear. Este tipo de energía determina la dinámica de los procesos atmosféricos y el clima. 
+    p.mb-4(data-aos="fade-down") Además de ser fuente de energía, la radiación solar brinda para los seres vivos los siguientes efectos fisiológicos positivos: 
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-down-left")
+      .col-12.mb-4.mb-md-0
+        ul.lista-ul.mb-2
+          li.mb-0
+            i.fas.fa-bolt(style="color:#AD84FF")
+            | Estimula la síntesis de vitamina D, que previene el raquitismo y la osteoporosis.
+          li.mb-0
+            i.fas.fa-bolt(style="color:#AD84FF")
+            | Favorece la circulación sanguínea. 
+          li.mb-0
+            i.fas.fa-bolt(style="color:#AD84FF")
+            | Estimula la síntesis de los neurotransmisores cerebrales responsables del estado anímico.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-12.col-lg-10.mb-4.mb-md-0(data-aos="fade-left")
+        .tarjeta.color-primario.p-3
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-1
+              img(src="@/assets/curso/temas/descargar.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.text-small Para conocer más detalles acerca de los aspectos teóricos del sol y la radiación se debe consultar el material complementario elaborado por el IDEAM (2016): 
+                .col-sm-auto
+                  a.boton.color-acento-botones(:href="obtenerLink('/downloads/2_1_1_MC_Teoria Radiacion.pdf')" target="_blank")
+                    span Descargar
+                    i.fas.fa-file-download
+
+    p.mb-4(data-aos="fade-down") Para la evaluación del potencial energético de la radiación solar, existen dos variables fundamentales:
+
+    .row.mb-5.align-self-stretch.justify-content-center.align-items-center
+      .col-12.col-md-12.mb-5(data-aos="fade-right")
+        figure
+          img(src='@/assets/curso/temas/tema2/tema2-06.svg', alt='') 
+      .col-12.col-md-12.mb-5(data-aos="fade-right")
+        figure
+          img(src='@/assets/curso/temas/tema2/tema2-07.svg', alt='') 
+
+    p.mb-4(data-aos="fade-down") Aunque sus nombres son parecidos, no se debe confundir los valores de la irradiancia con los valores de la irradiación. Haciendo un ejercicio mental con el equivalente eléctrico (físicamente no tienen equivalencia es solo para una mejor interpretación de los conceptos), la irradiancia puede asemejarse a la potencia eléctrica en cualquier dispositivo como por ejemplo un bombillo LED de 10 Vatios (10 W).
+
+    .row.justify-content-center.align-items-center.mb-3
+      .col-12(data-aos="fade-right")
+        ul.lista-ul.mb-2
+          li.mb-0
+            i.fas.fa-play(style="color:#25FFFF")
+            | Más información respecto a este tema podrá consultarla en el siguiente recurso:
+
+
+
+
 
 </template>
 
